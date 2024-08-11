64f57325445bc95992d9fd68a41b60ea6126107f
Santosh Basnet <12200163@students.koi.edu.au>
2024-08-11 23:06:55 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int result = AddNumbers(num1, num2);
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + result);
+        }
+
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
